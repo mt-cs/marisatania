709dbea481f21ad4315b45cf865a95e8105a45bf
Marisa Tania <mtania@dons.usfca.edu>
2022-12-05 09:30:49 -0800

    Update ethical-fashion-price-small-multiples-male.js

--- ethical-fashion/assets/js/d3/ethical-fashion-price-small-multiples-male.js
+++ ethical-fashion/assets/js/d3/ethical-fashion-price-small-multiples-male.js
@@ -1,15 +1,15 @@
 // define margin and svg size
-const price_small_multiples_margin = { top: 10, bottom: 10, left: 50, right: 10 }
+const price_small_multiples_margin = { top: 10, bottom: 10, left: 80, right: 10 }
 const price_small_multiples_width = 500
 const price_small_multiples_height = 600
 
 const price_small_multiples_single_width = 220
 const price_small_multiples_single_height = 130
 
-const small_multiples_dimensions = ['Tops','Bottom','Outerwear','Footwear'];
+const small_multiples_dimensions = ['Top','Tops','Bottom','Outerwear','Footwear'];
 
 // male stats
-d3.csv("https://gist.githubusercontent.com/mt-cs/7a7a015f377f35babfc698386dc2ec65/raw/e1d3b06b477815e502671ee32b4aa6fdbc7096d4/male-price.csv").then( function(data) {
+d3.csv("https://gist.githubusercontent.com/mt-cs/7a7a015f377f35babfc698386dc2ec65/raw/5829bc558a37bb2a95030705efb40b0257556974/male-price.csv").then( function(data) {
     const small_multiples_y_scales = []
     for (let i = 0; i < small_multiples_dimensions.length; i++) {
         small_multiples_y_scales.push(
@@ -59,9 +59,9 @@ d3.csv("https://gist.githubusercontent.com/mt-cs/7a7a015f377f35babfc698386dc2ec6
             )
             .attr('fill', (d, i) => {
                 if (i === 0) {
-                    return 'turquoise'
+                    return '#8dd3c7'
                 }
-                return 'salmon'
+                return '#fb8072'
             })
 
         d3.select('.male-bar-' + dimension)
@@ -72,59 +72,14 @@ d3.csv("https://gist.githubusercontent.com/mt-cs/7a7a015f377f35babfc698386dc2ec6
             .attr('x', (d, c) => 60 * c + 23)
             .attr('y', d => small_multiples_y_scales[i](+d[dimension]) - 10)
             .attr('width', 45)
-            .text(d => d[dimension])
+            .text(d => "$" + d[dimension])
             .attr('font-size', '12px')
 
         d3.select('.male-bar-' + dimension)
             .append('text')
-            .attr('x', 50)
+            .attr('x', 40)
             .attr('y', price_small_multiples_single_height - 15)
             .text(dimension)
             .attr('font-size', '14px')
     }
-
-    // legends
-    const male_small_multiples_legends = d3
-        .select('#male-small-multiples-legends')
-        .append('svg')
-        .attr(
-            'width',
-            price_small_multiples_width +
-            price_small_multiples_margin.left +
-            price_small_multiples_margin.right
-        )
-        .attr('height', 30)
-        .append('g')
-        .attr(
-            'transform',
-            'translate(' + price_small_multiples_width / 3 + ',' + 0 + ')'
-        )
-
-    male_small_multiples_legends
-        .append('rect')
-        .attr('x', 10)
-        .attr('y', 10)
-        .attr('width', 15)
-        .attr('height', 15)
-        .attr('fill', 'turquoise')
-
-    male_small_multiples_legends
-        .append('text')
-        .attr('x', 40)
-        .attr('y', 23)
-        .text('Eco Clothing')
-
-    male_small_multiples_legends
-        .append('rect')
-        .attr('x', 130)
-        .attr('y', 10)
-        .attr('width', 15)
-        .attr('height', 15)
-        .attr('fill', 'salmon')
-
-    male_small_multiples_legends
-        .append('text')
-        .attr('x', 160)
-        .attr('y', 23)
-        .text('Average Market')
 })
